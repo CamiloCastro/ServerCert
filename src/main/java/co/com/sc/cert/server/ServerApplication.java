a9b8712c4ebb15087da89e33dcd3e87cd92fb421
juan-castro <juan.castro@software-colombia.com>
2017-12-21 14:39:44 -0500

    Add BC Provider

--- src/main/java/co/com/sc/cert/server/ServerApplication.java
+++ src/main/java/co/com/sc/cert/server/ServerApplication.java
@@ -1,14 +1,18 @@
 package co.com.sc.cert.server;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.security.Security;
 
 @SpringBootApplication
 @EntityScan(basePackages= {"co.com.sc.cert.server.model.entities"})
 public class ServerApplication
 {
     public static void main(String[] args) {
+        Security.addProvider(new BouncyCastleProvider());
         SpringApplication.run(ServerApplication.class, args);
     }
 }
