ef30140b2b29ada4fc97bbd782e91295863d8168
juan-castro <juan.castro@software-colombia.com>
2017-12-21 17:27:48 -0500

    Fix a bug

--- src/main/java/co/com/sc/cert/server/controller/SignController.java
+++ src/main/java/co/com/sc/cert/server/controller/SignController.java
@@ -6,7 +6,6 @@ import co.com.sc.cert.server.model.request.WSResponse;
 import co.com.sc.cert.server.repository.CertificateRepository;
 import co.com.sc.cert.server.service.AuthenticationService;
 import co.com.sc.cert.server.util.S3Functions;
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
